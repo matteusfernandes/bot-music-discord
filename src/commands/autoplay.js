376f2f2abafb7989f9a94c59bb4191a4945944ed
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-06-21 19:20:45 -0300

    refactor: remove unnecessary statement

--- src/commands/autoplay.js
+++ src/commands/autoplay.js
@@ -3,7 +3,7 @@ module.exports = {
   inVoiceChannel: true,
   run: async (client, message) => {
     const queue = client.distube.getQueue(message);
-    if (!queue) return message.channel.send(`${client.emotes.error} | A fila está vazia!`);
+    if (!queue) message.channel.send(`${client.emotes.error} | A fila está vazia!`);
     const autoplay = queue.toggleAutoplay();
     message.channel.send(`${client.emotes.success} | AutoPlay: \`${autoplay ? 'On' : 'Off'}\``);
   }
